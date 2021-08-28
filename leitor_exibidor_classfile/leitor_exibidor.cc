e3db1bc77d674445c1cf8847e6ff0c494fd69984
João Moraes <joaoadm94@gmail.com>
2021-08-28 20:14:23 -0300

    leitura das versoes minor, major

--- leitor_exibidor_classfile/leitor_exibidor.cc
+++ leitor_exibidor_classfile/leitor_exibidor.cc
@@ -2,10 +2,28 @@
 #include <fstream>
 #include <iostream>
 #include <netinet/in.h>
+#include <string.h>
 
 #define magic_number 0xcafebabe
 
 using namespace std;
+
+int convert(char num[]) {
+   int len = strlen(num);
+   int base = 1;
+   int temp = 0;
+   for (int i=len-1; i>=0; i--) {
+      if (num[i]>='0' && num[i]<='9') {
+         temp += (num[i] - 48)*base;
+         base = base * 16;
+      }
+      else if (num[i]>='A' && num[i]<='F') {
+         temp += (num[i] - 55)*base;
+         base = base*16;
+      }
+   }
+   return temp;
+}
 
 class ClassFile {
     public:
@@ -27,7 +45,8 @@ class ClassFile {
         uint16_t attribute_count;
         uint8_t * attribute_table;
         void carregarArquivo(char* nomeArquivo);
-        void lerArquivo();
+        void lerNumeroMagico();
+        void lerMinorMajor();
 };
 
 void ClassFile::carregarArquivo(char* nomeArquivo) {
@@ -42,37 +61,49 @@ void ClassFile::carregarArquivo(char* nomeArquivo) {
     }
 }
 
-void ClassFile::lerArquivo() {
-    if (arquivo.is_open()) {
-        uint32_t buffer;
-        arquivo.read(reinterpret_cast<char *>(&buffer), sizeof(buffer));
+void ClassFile::lerNumeroMagico() {
+    uint32_t buffer;
+    arquivo.read(reinterpret_cast<char *>(&buffer), sizeof(buffer));
 
-        /** htonl() - host to network long conversion:
-         * realiza a conversao de inteiros multi-byte 
-         * da ordem do host para a ordem da rede
-         * ex: se o programa ler "bebafeca", esse comando corrige
-         * a ordem para "cafebabe" 
-         **/
-        buffer = htonl(buffer);
-        cafebabe = buffer;
-        
-        // captura configuracoes do cout para posterior restauracao 
-        ios_base::fmtflags oldFlags = cout.flags();
-        streamsize oldPrec = cout.precision();
-        char oldFill = cout.fill();
-        
-        cout << "Magic: " << hex << cafebabe << "\n";
-        
-        // restaura configuracoes do cout apos o uso do parametro "hex"
-        cout.flags(oldFlags);
-        cout.precision(oldPrec);
-        cout.fill(oldFill);
+    /** htonl() - host to network long conversion:
+     * realiza a conversao de inteiros multi-byte 
+     * da ordem do host para a ordem da rede
+     * ex: se o programa ler "bebafeca", esse comando corrige
+     * a ordem para "cafebabe" 
+     **/
+    buffer = htonl(buffer);
+    cafebabe = buffer;
+    
+    // captura configuracoes do cout para posterior restauracao 
+    ios_base::fmtflags oldFlags = cout.flags();
+    streamsize oldPrec = cout.precision();
+    char oldFill = cout.fill();
+    
+    cout << "Magic: " << hex << cafebabe << "\n";
+    
+    // restaura configuracoes do cout apos o uso do parametro "hex"
+    cout.flags(oldFlags);
+    cout.precision(oldPrec);
+    cout.fill(oldFill);
 
-        if (cafebabe == magic_number) {
-            cout << "O arquivo possui o número magico de um .class \n";
-        }
-
+    if (cafebabe == magic_number) {
+        cout << "O arquivo possui o número magico de um .class \n";
     }
+}
+
+void ClassFile::lerMinorMajor() {
+    
+    uint16_t buffer;
+
+    arquivo.read(reinterpret_cast<char *>(&buffer), sizeof(buffer));
+    buffer = htons(buffer);
+    minor = buffer;
+
+    arquivo.read(reinterpret_cast<char *>(&buffer), sizeof(buffer));
+    buffer = htons(buffer);
+    major = buffer;
+
+    cout << "Version (major.minor): " << major << "." << minor << "\n";
 }
 
 int main(int argc, char* argv[]) {
@@ -86,7 +117,10 @@ int main(int argc, char* argv[]) {
     ClassFile cf;
 
     cf.carregarArquivo(argv[1]);
-    cf.lerArquivo();
+    if(cf.arquivo.is_open()) {
+        cf.lerNumeroMagico();
+        cf.lerMinorMajor();
+    }
 
     return 0;
 }
