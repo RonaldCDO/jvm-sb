9deb9d0bdd6f2928d30142ee28205777dbb25c2c
João Moraes <joaoadm94@gmail.com>
2021-08-28 18:01:00 -0300

    criada classe para armazenar os dados

--- leitor_exibidor_classfile/leitor_exibidor.cc
+++ leitor_exibidor_classfile/leitor_exibidor.cc
@@ -5,6 +5,29 @@ using namespace std;
 
 #define u4length 4
 
+class ClassFile {
+    public:
+        uint8_t ca;
+        uint8_t fe;
+        uint8_t ba;
+        uint8_t be;
+        uint16_t minor;
+        uint16_t major;
+        uint16_t constant_pool_count;
+        uint8_t * constant_pool_table;
+        uint16_t acess_flags;
+        uint16_t this_class;
+        uint16_t super_class;
+        uint16_t interface_count;
+        uint8_t * interface_table;
+        uint16_t field_count;
+        uint8_t * field_table;
+        uint16_t method_count;
+        uint8_t * method_table;
+        uint16_t attribute_count;
+        uint8_t * attribute_table;  
+};
+
 void carregarArquivo(char* nomeArquivo) {
 
     cout << nomeArquivo << "\n";
