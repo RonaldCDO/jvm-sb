59659ef22fb710fdf446c45d9182bab34d010406
ronaldcdo <ronaldcesar.eng@gmail.com>
2021-09-03 15:45:22 -0300

    Adding Fields.h

---
+++ leitor_exibidor_classfile/include/fields.h
@@ -0,0 +1,21 @@
+#include "data_class_format.h"
+
+#define ACC_PUBLIC "0x0001"
+#define ACC_PRIVATE "0x0002"
+#define ACC_PROTECTED "0x0004"
+#define ACC_STATIC "0x0008"
+#define ACC_FINAL "0x0010"
+#define ACC_VOLATILE "0x0040"
+#define ACC_TRANSIENT "0x0080"
+#define ACC_SYNTHETIC "0x1000"
+#define ACC_ENUM "0x4000"
+
+class Fields_info{
+    protected:
+        u2 access_flags;
+        u2 name_index;
+        u2 descriptor_index;
+        u2 attributes_count;
+    public:
+        
+};
